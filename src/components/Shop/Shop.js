14e4bfd1b8dc9a1cfd1483fad02806d63a0ec099
Jahirul Islam <jahirul@elyranur.com>
2022-11-12 00:32:00 +0600

    first commit

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+    const [products, setfProducts] = useState([])
+    const [cart, setCart] = useState([])
+
+    useEffect( () =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setfProducts(data))
+    }, [])
+    const handleAddToCart = (product) =>{
+        // cart.push(product)
+
+        const newCArt = [...cart, product]
+        setCart(newCArt)
+    }
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product 
+                        key={product.id}
+                        product={product}
+                        handleAddToCart = {handleAddToCart}
+                        ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
